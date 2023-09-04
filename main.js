a345517cc56c4f0ba15df9b5b775da2442212f3e
mazur-krzysiek <mazur.krzysiek@yahoo.com>
2023-09-04 06:02:25 +0200

    mobile nav toggle menu

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".navigation-primary");
 
 navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute('data-visible') 
-  ? navToggle.setAttribute("aria-expanded", false)
-  : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
 });
